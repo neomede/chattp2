9ea9efdb0cc5dffff301ffa18b59bfa8a984470a
Rubén Simón Andreo <rsimonandreo@gmail.com>
2018-01-25 14:44:39 +0100

    check codeclimate coverage

--- src/chattp2/client_test.go
+++ src/chattp2/client_test.go
@@ -14,3 +14,10 @@ func TestNewClient(t *testing.T) {
 	a.NoError(err)
 	a.NotNil(client)
 }
+
+func TestTestedFunc(t *testing.T) {
+	a := assert.New(t)
+
+	result := chattp2.TestedFunc()
+	a.True(result)
+}
